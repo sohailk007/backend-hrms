d8b31541b9d78d942573ea39d3ba4b5301d40f11
Md Sohail Ali <sohailt7x@gmail.com>
2026-02-04 21:12:05 +0530

    migration

--- backend/settings.py
+++ backend/settings.py
@@ -49,6 +49,8 @@ else:
     CORS_ALLOWED_ORIGINS = [
         "http://localhost:5173",
         "http://127.0.0.1:5173",
+        'https://frontend-hrms-omega.vercel.app',
+        
     ]
 
 CORS_ALLOW_CREDENTIALS = True
